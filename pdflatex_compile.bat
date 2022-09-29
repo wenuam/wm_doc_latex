9aa636753d1c399e170714139429fa5f73374e1b
wenuam <david.koch@wenuam.fr>
2022-09-29 21:13:56 +0200

    Update compiler wrapper (start directory selection)

--- pdflatex_compile.bat
+++ pdflatex_compile.bat
@@ -1,10 +1,22 @@
 @echo off
 
-rem Set this batch's folder as current folder (in case of drag'n drop use)
-set "cd=%~dp0"
+rem Start directory, always the batch's folder
+set "sd=%~dp0"
+
+rem Set destination folder as current folder (start if no drag'n drop)
+if "%~1"=="" (
+	set "cd=%~dp0"
+) else (
+	set "cd=%~dp1"
+)
+
+if "%cd:~-1%"=="\" set "cd=%cd:~0,-1%"
+
+rem Enforce current working directory (i.e. for Total Commander, if placed into a menu bar)
+cd /d "%cd%"
 
 rem Set PATH with tools
-set "PATH=%PATH%;%cd%\inkscape\bin;%cd%\latex;%cd%\miktex\texmfs\install\miktex\bin\x64;%cd%\nvm;%cd%\nvm\v16.13.0;%cd%\pandoc;%cd%\perl\perl\bin;%cd%\tools\ditaa;%cd%\tools\msc-generator;"
+set "PATH=%PATH%;%sd%\inkscape\bin;%sd%\latex;%sd%\miktex\texmfs\install\miktex\bin\x64;%sd%\nvm;%sd%\nvm\v16.13.0;%sd%\pandoc;%sd%\perl\perl\bin;%sd%\tools\ditaa;%sd%\tools\msc-generator;"
 set "PATH=%PATH:\\=\%"
 set "PATH=%PATH:;;=;%"
 set "PATH=%PATH: ;=;%"
