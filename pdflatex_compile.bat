b6fc364522f9b4ee5b0b6d776d9ec2515dfa2756
Kochise <david.koch@9online.fr>
2023-01-08 18:53:43 +0100

    Fix if EOL is LF See git core.autocrlf

--- pdflatex_compile.bat
+++ pdflatex_compile.bat
@@ -1,4 +1,11 @@
-@echo off && setlocal enabledelayedexpansion && chcp 65001>nul
+@echo off && setlocal enabledelayedexpansion
+if "%~dp0" neq "%tmp%\" (set "cd=%~dp0" & (if not exist "%tmp%\%~nx0" (find "" /v<"%~f0" >"%tmp%\%~nx0")) & call "%tmp%\%~nx0" %* & del "%tmp%\%~nx0" 2>nul & exit /b) else (if "%cd:~-1%"=="\" set "cd=%cd:~0,-1%")
+
+rem Compile a LaTeX document into PDF, by wenuam 2022
+
+rem Set code page to utf-8 (/!\ this file MUST be in utf-8)
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+chcp 65001>nul
 
 rem Change default helpers
 set "quiet=1>nul 2>nul"
@@ -7,23 +14,32 @@ set "fquiet=/f /q 1>nul 2>nul"
 rem Runner loop
 set /a tries=0
 
-rem Start directory, always the batch's folder
-set "sd=%~dp0"
+rem Start directory, always the batch's folder (%~dp0)
+set "sd=%cd%"
 
 rem Set destination folder as current folder (start if no drag'n drop)
-if "%~1"=="" (
-	set "cd=%~dp0"
-) else (
+if "%~1" neq "" (
 	set "cd=%~dp1"
 )
 
+rem Fix path ending
 if "%cd:~-1%"=="\" set "cd=%cd:~0,-1%"
 
 rem Enforce current working directory (i.e. for Total Commander, if placed into a menu bar)
 cd /d "%cd%"
 
 rem Set PATH with tools
-set "PATH=%PATH%;%sd%\inkscape\bin;%sd%\latex;%sd%\miktex\texmfs\install\miktex\bin\x64;%sd%\nvm;%sd%\nvm\v16.13.0;%sd%\pandoc;%sd%\perl\perl\bin;%sd%\tools\ditaa;%sd%\tools\msc-generator;"
+set "PATH=%PATH%;%sd%\inkscape\bin"
+set "PATH=%PATH%;%sd%\latex"
+set "PATH=%PATH%;%sd%\miktex\texmfs\install\miktex\bin\x64"
+set "PATH=%PATH%;%sd%\nvm"
+set "PATH=%PATH%;%sd%\nvm\v16.13.0"
+set "PATH=%PATH%;%sd%\pandoc"
+set "PATH=%PATH%;%sd%\perl\perl\bin"
+set "PATH=%PATH%;%sd%\tools\ditaa"
+set "PATH=%PATH%;%sd%\tools\msc-generator"
+
+rem Clean PATH
 set "PATH=%PATH:\\=\%"
 set "PATH=%PATH:;;=;%"
 set "PATH=%PATH: ;=;%"
@@ -49,14 +65,27 @@ if exist "%~1" (
 	)
 )
 
+rem Parameters
+set "vpar="
+set "vpar=%vpar% -shell-escape"
+set "vpar=%vpar% -synctex=1"
+set "vpar=%vpar% -interaction=nonstopmode"
+set "vpar=%vpar% --extra-mem-top=10000000"
+set "vpar=%vpar% -file-line-error"
+set "vpar=%vpar% -max-print-line=8190"
+
 :repeat
-if %tries% equ 0 (goto :eof)
+if %tries% equ 0 (goto :done)
 set /a tries-=1
 echo;
-echo; %time% ===================================================================
+echo === COMPILING : %tries% @ %time% ==========================================
 rem Generate PDF file (those arguments allow the magic to happen)
-pdflatex -shell-escape -synctex=1 -interaction=nonstopmode --extra-mem-top=10000000  -file-line-error -max-print-line=8190 "%~1"
+pdflatex %vpar% "%~1"
 if %errorlevel% equ 0 (goto :repeat) else (
 	echo FAILED (%errorlevel%) check the log...
 	pause
 )
+
+:done
+rem Restore saved code page
+chcp %cp%>nul
